41cbd272d307428fa36befe44e7df0cd2b7b70d2
Lavi <lavifb@gmail.com>
2017-07-31 17:53:16 -0700

    added hamil.c and hamil.h

---
+++ include/hamil.h
@@ -0,0 +1,15 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "block.h"
+
+double *HeisenH_int(const double* H_params, const int dim1, const int dim2, 
+					const double *restrict Sz1, const double *restrict Sp1, 
+					const double *restrict Sz2, const double *restrict Sp2);
+
+double *HeisenH_int_r(const double* H_params, const int dim1, const int dim2, 
+					const double *restrict Sz1, const double *restrict Sp1, 
+					const double *restrict Sz2, const double *restrict Sp2, 
+					const int num_ind, const int *restrict inds);
+
+#endif
