7f978daa37850177be305502955f7bcc855a21d7
Lavi <lavifb@gmail.com>
2018-01-24 11:55:16 -0800

    made measuring functions in meas.c

--- include/meas.h
+++ include/meas.h
@@ -1,6 +1,7 @@
 #ifndef MEAS_H
 #define MEAS_H
 
+#include "block.h"
 #include "linalg.h"
 #include <stdio.h>
 #include <time.h>
@@ -21,5 +22,9 @@ meas_data_t *createMeas(int num_sites);
 void freeMeas(meas_data_t *meas);
 
 int outputMeasData(const char* path, meas_data_t *meas);
+
+void measureSzs(DMRGBlock *sys_enl, int dimEnv, MAT_TYPE *psi, int Sz_mat_offset, meas_data_t *meas);
+
+void measureSSs(DMRGBlock *sys_enl, int dimEnv, MAT_TYPE *psi, int Sz_mat_offset, meas_data_t *meas);
 
 #endif
